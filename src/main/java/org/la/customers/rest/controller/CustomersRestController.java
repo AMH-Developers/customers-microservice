2a0aaca017e2b1a3847b1750125dd10f29e72bd6
Hend <haide90@hotmail.com>
2021-05-12 00:38:25 +0800

    creating findAll method

--- src/main/java/org/la/customers/rest/controller/CustomersRestController.java
+++ src/main/java/org/la/customers/rest/controller/CustomersRestController.java
@@ -1,5 +1,11 @@
 package org.la.customers.rest.controller;
 
+import java.util.List;
+
+import org.la.customers.model.CustomersModel;
+import org.la.customers.service.CustomersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customers")
 public class CustomersRestController {
 	
+	@Autowired
+	private CustomersService customersService;
+	
+	@GetMapping("/findAll")
+	public List<CustomersModel> findAll() {
+		return customersService.findAll();
+	}
 	
 
 	
--- src/main/java/org/la/customers/service/CustomersService.java
+++ src/main/java/org/la/customers/service/CustomersService.java
@@ -1,9 +1,23 @@
 package org.la.customers.service;
 
+import java.util.List;
+
+import org.la.customers.model.CustomersModel;
+import org.la.customers.repository.CustomersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomersService {
+
+	@Autowired
+	private CustomersRepository customersRepository;
+	
+	
+	public List <CustomersModel> findAll() {
+		List<CustomersModel> customersList = customersRepository.findAll();
+		return customersList;
+	}
 	
 	
 
